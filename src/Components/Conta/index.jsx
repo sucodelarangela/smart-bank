3a806b7c50e35b0c11fb459f7674c0c1d78abc47
Angela Caldas <aes.caldas@gmail.com>
2022-08-08 13:35:39 -0300

    full refactor of existing css to styled-components

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
+
+// importando o Icone global para uso no lugar de <img className="imagem-icone"/>
+import { Icone, Box, Botao, Saldo, Detalhe } from "../UI";
+
+// componente que irá herdar estilos de Icone. Dessa forma, podemos adicionar estilizações específicas para um determinado elemento sem aplicar as mesmas nos demais que seriam iguais
+const IconeMargin = styled(Icone)`
+margin-top: 2px;
+`;
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
@@ -12,29 +21,27 @@ const Conta = () => {
   };
 
   return (
-    <div className="box">
+    <Box>
       <h2>Conta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <img className="imagem-icone" src={dinheiro} alt="Ícone Saldo" />
+          <Icone src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalhe">R$</span> 0,00{" "}
-          </div>
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagem-icone"
+      <Botao onClick={toggleHandler}>
+        <IconeMargin
           src={toggleState ? privado : olho_icone}
           alt="Privacidade do Saldo"
         />
-      </button>
-    </div>
+      </Botao>
+    </Box>
   );
 };
 
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 
+import styled from 'styled-components';
+
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
-const Container = () => {
+const Container = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+
+// para evitar erros com componente e estilos usando mesmo nome de variável, vamos transformar o componente abaixo em uma função anônima
+// const Container = () => {
+export default () => {
   return (
-    <div className="container">
+    <Container>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </Container>
   );
 };
+// Uma outra solução seria renomear o componente de Container para ContainerWrapper, por exemplo
 
-export default Container;
+// este export pode ser excluído
+// export default Container;
